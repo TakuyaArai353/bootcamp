3599e4712528e011b33652b986338345c63fcf54
Takuya Arai <84216364+TakuyaArai353@users.noreply.github.com>
2023-04-08 16:38:20 +0900

    Update conditional_statements_loops.sh
    インデントを2半角に修正しました。

--- shell/conditional_statements_loops.sh
+++ shell/conditional_statements_loops.sh
@@ -3,10 +3,10 @@
 for i in $(seq 1 15)
   do
     if [ $i -ge 10 ]; then
-	  echo $i "は10以上です。"
+      echo $i "は10以上です。"
     elif [ $i -ge 5 ] ; then
-	  echo $i "は5以上10以下です。"
+      echo $i "は5以上10以下です。"
     else
-	  echo $i "は5以下です。"
+      echo $i "は5以下です。"
   fi
 done
