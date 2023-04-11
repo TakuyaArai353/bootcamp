5c42813ea3656cddb09e137040b3a8a5949e7834
TakuyaArai353 <bonsunsogo@gmail.com>
2023-04-11 22:01:06 +0900

    add: Stringクラスの利用例をコード

---
+++ ruby/string.rb
@@ -0,0 +1,14 @@
+# 文字列の生成
+string1 = "Hello, World!"
+string2 = 'Hello, World!'
+
+# 文字列の連結
+string3 = "Hello" + " " + "World!"
+
+# 文字列の検索
+string1.include?("Hello")
+string1.start_with?("Hello")
+
+# 文字列の変換
+string4 = "123"
+number = string4.to_i
